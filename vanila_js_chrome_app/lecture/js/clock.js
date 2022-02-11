696f74f0bd1259d53181a0126e632e284864c889
foreverhary <hongkeon.kim@gmail.com>
2022-02-11 15:52:22 +0900

    vanila challenge update and lecture study

--- vanila_js_chrome_app/lecture/js/clock.js
+++ vanila_js_chrome_app/lecture/js/clock.js
@@ -1,12 +1,16 @@
-const clock = document.querySelector("h2#clock");
+const clock = document.querySelector("h2");
 
-function sayHello() {
-  console.log(Date.toString("yyyy:mm:dd hh:MM:ss"));
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()} ${hours}:${minutes}:${seconds}`;
 }
 function sayTimeout() {
   console.log("timeout!!");
 }
 
-setInterval(sayHello, 1000);
+setInterval(getClock, 1000);
 
 setTimeout(sayTimeout, 1000);
